302d30c0bf9d84cfce91d0ddf79d0b9d6b19834f
wchamber01 <williamchambers@gmail.com>
2020-01-15 20:11:38 -0500

    setting up routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require("./server");
 
-server.listen(5000, () => {
-  console.log("\n* Server Running on http://localhost:5000 *\n");
+server.listen(5001, () => {
+  console.log("\n* Server Running on http://localhost:5001 *\n");
 });
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,58 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const Users = require("./userDb");
+
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+//GET all users
+router.get("/", (req, res) => {
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error getting users" });
+    });
+});
+
+//GET single user by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params.id;
+  Users.getById(id)
+    .then(user => {
+      if (!user.id) {
+        res.status(404).json({ error: "The specified user does not exist." });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+//GET all posts by user id
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
